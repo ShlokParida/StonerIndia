529e74417324fe33a38da83bd6c2d42bab8262c5
root <shlokparida@gmail.com>
2019-11-11 03:22:14 +0530

    local import

--- StonerIndia/settings.py
+++ StonerIndia/settings.py
@@ -136,3 +136,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+
+except (ImportError):
+    pass
+
+    
