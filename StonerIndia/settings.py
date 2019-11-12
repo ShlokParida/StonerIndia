e0aa18c56c077c7356eb746412e4b23403c42d2c
root <shlokparida@gmail.com>
2019-11-12 22:17:58 +0000

    Revert "Update"
    This reverts commit 59eb230bb276762ca66baabf02d7e241b2ae6283.

--- StonerIndia/settings.py
+++ StonerIndia/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l(@uj-kk((4wgjfkcy83*!y9vonrm9bmne!nnqy^rg991as$ti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['www.stonerindia.website' , '139.59.89.198' ]
 
 
 # Application definition
